ed376237ee83723f7a2ffdceb6215440f15b1cc1
shivam mishra <shivambmishra10@gmail.com>
2023-01-07 02:36:03 +0530

    files added

---
+++ frontend/src/store.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { configureStore } from "@reduxjs/toolkit";
+import {
+  allUsersReducer,
+  postOfFollowingReducer,
+  userProfileReducer,
+  userReducer,
+} from "./Reducers/User";
+import { likeReducer, myPostsReducer, userPostsReducer } from "./Reducers/Post";
+
+const store = configureStore({
+  reducer: {
+    user: userReducer,
+    postOfFollowing: postOfFollowingReducer,
+    allUsers: allUsersReducer,
+    like: likeReducer,
+    myPosts: myPostsReducer,
+    userProfile: userProfileReducer,
+    userPosts: userPostsReducer,
+  },
+});
+
+export default store;
